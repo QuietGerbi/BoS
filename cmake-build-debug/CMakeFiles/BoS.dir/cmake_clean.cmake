be3f38dd5fe5583322bd0c4c932b12b404df2749
QuietGerbi <arhipos497@gmail.com>
2026-09-10 00:17:11 +0700

    вынес файлики отдельно от раздела

---
+++ cmake-build-debug/CMakeFiles/BoS.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BoS"
+  "BoS.pdb"
+  "CMakeFiles/BoS.dir/1_vol/main.c.o"
+  "CMakeFiles/BoS.dir/1_vol/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BoS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
